f86bceb2c4ef13bbe4d90ec5558a0b472b08e280
Gregory Avery-Weir <gregory@futureproofgames.com>
2015-06-03 13:27:01 -0400

    Make form selections change route

--- app/traps/traps.js
+++ app/traps/traps.js
@@ -3,22 +3,31 @@
 angular.module('5eGenerator.traps', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/traps', {
+  $routeProvider.when('/traps/:level?/:danger?', {
     templateUrl: 'traps/traps.html',
     controller: 'TrapsCtrl'
   });
 }])
 
-.controller('TrapsCtrl', ['$scope', 'trapData',
-    function($scope, trapData) {
+.controller('TrapsCtrl', ['$scope', 'trapData', '$routeParams', '$location',
+  function($scope, trapData, $routeParams, $location) {
 
-    $scope.trapDangers = trapData.dangers;
-    $scope.trapSaves = trapData.saveDC;
-    $scope.trapAttack = trapData.attackBonus;
-    $scope.trapDamage = trapData.damageSeverity;
+  $scope.trapDangers = trapData.dangers;
+  $scope.trapSaves = trapData.saveDC;
+  $scope.trapAttack = trapData.attackBonus;
+  $scope.trapDamage = trapData.damageSeverity;
 
-    $scope.trap = {
-        level: 1,
-        danger: "Setback"
-    };
+  $scope.trap = {
+    level: $routeParams.level ? parseInt($routeParams.level) : 1,
+    danger: (
+        $routeParams.danger &&
+        trapData.dangers.indexOf($routeParams.danger) > -1)
+      ? $routeParams.danger : "Setback"
+  };
+
+  $scope.$watch('trap', function(newVal) {
+    if(parseInt(newVal.level) && trapData.dangers.indexOf(newVal.danger) > -1) {
+      $location.path('/traps/' + newVal.level + '/' + newVal.danger)
+    }
+  }, true);
 }]);
